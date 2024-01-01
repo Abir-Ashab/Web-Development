de0fccca9a058d75a2229e061897879b8709a41e
Abir Ashab Niloy <113261051+Abir-Ashab@users.noreply.github.com>
2024-01-02 01:52:24 +0600

    Add files via upload

---
+++ CV/profile.js
@@ -0,0 +1,6 @@
+const profileText = "I am a 3rd year student of IIT, University of Dhaka. Here I am pursuing my bachelor in software engineering. I love problem solving and get dopamine from it as well. I want to work on real-world projects with my problem-solving and development skills, and I'm always ready to learn new stuffs!";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const profileElement = document.getElementById("profile");
+    profileElement.textContent = profileText;
+});
